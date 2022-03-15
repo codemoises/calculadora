abdd3cd045bd42fea011b4e9457228474bb02777
Moises Cruz <moisesbraga223@gmail.com>
2022-03-15 16:42:22 -0300

    Atualizado!

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@ function insert(num) {
 }
 
 function clean() {
-    document.getElementById('resultado').innerHTML = "";
+    document.getElementById('resultado').innerHTML = '';
 }
 
 function back() {
@@ -18,8 +18,8 @@ function back() {
 function calcular() {
     var resultado = document.getElementById('resultado').innerHTML;
     if (resultado) {
-        document.getElementById('resultado').innerHTML = eval(resultado)
+        document.getElementById('resultado').innerHTML = eval(resultado);
     } else {
-        document.getElementById('resultado').innerHTML = "Undefined."
+        document.getElementById('resultado').innerHTML = '';
     }
 }
